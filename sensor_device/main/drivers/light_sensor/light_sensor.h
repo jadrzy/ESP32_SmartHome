93345205db7d4c9de3c081515b5711f71fcdecfd
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-09-21 22:21:20 +0200

    Update light_sensor.h

--- sensor_device/main/drivers/light_sensor/light_sensor.h
+++ sensor_device/main/drivers/light_sensor/light_sensor.h
@@ -7,26 +7,34 @@
 #ifndef HEADER_LIGHT_SENSOR_H
 #define HEADER_LIGHT_SENSOR_H
 
+// Timeout for I2C transactions related to the light sensor (in milliseconds)
 #define I2C_TIMEOUT_LIGHT_SENSOR 100
 
+// Command sequence for configuring the light sensor over I2C
 static const uint8_t command_light_sensor[][2] = {
     
-    // TIME
-    {   0x81,       // TIMING REG
-        0x10,   },  // GAIN = x1    INTEGRATION TIME = 402ms
+    // Timing configuration
+    {   
+        0x81,   // Timing register
+        0x10,   // GAIN = 1x, INTEGRATION TIME = 402ms
+    },  
 
-    // INTERRUPT
-    {   0x86,       // INTERRUPT REG
-        0x00,   },  // DISABLE INTERRUPT
+    // Interrupt configuration
+    {   
+        0x86,   // Interrupt register
+        0x00,   // Disable interrupts
+    },
 
-    // POWER UP
-    {   0x80,       // CONTROL REG
-        0x03,   },  // POWER ON
-
+    // Power up the sensor
+    {   
+        0x80,   // Control register
+        0x03,   // Power on
+    },
 };
 
-void light_sensor_config(i2c_master_dev_handle_t * sensor_handle);
-void read_light_sensor(i2c_master_dev_handle_t * sensor_handle, uint32_t * lux); 
-unsigned int CalculateLux(unsigned int iGain, unsigned int tInt, unsigned int ch0, unsigned int ch1);
+// Function declarations
+void light_sensor_config(i2c_master_dev_handle_t * sensor_handle);  // Configure the light sensor
+void read_light_sensor(i2c_master_dev_handle_t * sensor_handle, uint32_t * lux);  // Read light sensor data
+static unsigned int CalculateLux(unsigned int iGain, unsigned int tInt, unsigned int ch0, unsigned int ch1);  // Calculate lux from raw sensor data
 
 #endif
