245eef0ff037a82a69d24542d595497eb0ed7ebc
jchlopak <jdrzyzga10@mgmail.com>
2024-10-07 11:20:07 +0200

    partitions

--- master_device/main/partitions/partitions.c
+++ master_device/main/partitions/partitions.c
@@ -1,11 +1,11 @@
-#include "master_device.h"
+#include "partitions.h"
 
 static uint64_t assigned_devices[10] = {0};
 
 
-void init_nvs(uint64_t * dev)
+void init_nvs(void)
 {
-    esp_err_t ret = nvs_flash_init();
+esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
     {
         ESP_ERROR_CHECK(nvs_flash_erase());
@@ -14,8 +14,3 @@ void init_nvs(uint64_t * dev)
     ESP_ERROR_CHECK(ret);
 }
 
-
-void app_main(void)
-{
-
-}
---
+++ master_device/main/partitions/partitions.h
@@ -0,0 +1,17 @@
+#ifndef PARTITIONS_HEADER
+#define PARTITIONS_HEADER
+
+#include <stdint.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+
+void init_nvs(void);
+
+#endif
