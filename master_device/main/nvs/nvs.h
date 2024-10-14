9b65ae13d259f92b4945c9082bde5eac672eb0cb
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-10-14 08:20:38 +0200

    Update nvs.h

--- master_device/main/nvs/nvs.h
+++ master_device/main/nvs/nvs.h
@@ -2,22 +2,19 @@
 #define PARTITIONS_HEADER
 
 #include <stdint.h>
-#include "esp_err.h"
-#include "master_device.h"
-#include "nvs_flash.h"
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_mac.h"
+#include "esp_err.h"
 #include "esp_log.h"
+#include "esp_mac.h"
 #include "nvs_flash.h"
+#include "master_device.h"
 
+// Function declarations
 void init_nvs_partitions(void);
-void get_master_serial_number_from_nvs(char * number);
+void get_master_serial_number_from_nvs(char *number);
 void get_slave_serial_number_from_nvs(char number[][SERIAL_NUMBER_SIZE + 1]);
 void write_slave_serial_number_to_nvs(char number[][SERIAL_NUMBER_SIZE + 1]);
-// void operation(void);
 
-#endif
+#endif // PARTITIONS_HEADER
