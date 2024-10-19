66a0f888de9bf18db8482c7270579eb0d98bce09
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-10-19 16:42:26 +0200

    Update wifi.c

--- master_device/main/components/wifi/wifi.c
+++ master_device/main/components/wifi/wifi.c
@@ -6,6 +6,28 @@
 #include "esp_wifi_types_generic.h"
 #include <stdio.h>
 #include <string.h>
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif_net_stack.h"
+#include "esp_netif.h"
+#include "nvs_flash.h"
+#include "lwip/inet.h"
+#include "lwip/netdb.h"
+#include "lwip/sockets.h"
+#include "lwip/lwip_napt.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+
+
+
 static const char *TAG_WIFI = "WIFI";
 
 // ADDED ////////////////////////////////////
