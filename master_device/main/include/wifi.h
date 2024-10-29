bc36eb7eaf64dbd478565fd413ef42fba5b22491
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-10-29 10:27:44 +0100

    Update wifi.h

--- master_device/main/include/wifi.h
+++ master_device/main/include/wifi.h
@@ -7,28 +7,36 @@
 
 #include <stdint.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
+#include <time.h>
 #include <assert.h>
 #include "esp_err.h"
-#include "esp_netif_types.h"
+#include "esp_wifi.h"
 #include "esp_wifi_default.h"
 #include "esp_wifi_types_generic.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
 #include "freertos/timers.h"
 #include "esp_event_base.h"
-#include "nvs_flash.h"
-#include "esp_random.h"
 #include "esp_event.h"
-#include "esp_netif.h"
-#include "esp_wifi.h"
+#include "esp_random.h"
 #include "esp_log.h"
 #include "esp_mac.h"
 #include "esp_now.h"
 #include "esp_crc.h"
-
-
+#include "esp_netif_net_stack.h"
+#include "esp_netif_types.h"
+#include "esp_netif.h"
+#include "lwip/def.h"
+#include "lwip/inet.h"
+#include "lwip/netdb.h"
+#include "lwip/sockets.h"
+#include "lwip/lwip_napt.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "nvs_flash.h"
 
 
 typedef struct {
