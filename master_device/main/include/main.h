d9eac018d5d59e033bb0394ff7846e97a2133f95
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-10-29 10:13:59 +0100

    Update main.h

--- master_device/main/include/main.h
+++ master_device/main/include/main.h
@@ -1,4 +1,21 @@
 #ifndef MASTER_DEVICE_HEADER
 #define MASTER_DEVICE_HEADER
 
+
+#include "include/components.h"
+#include "include/data.h"
+#include "include/nvs.h"
+#include "include/task.h"
+#include "include/wifi.h"
+
+
+
+#include "esp_err.h"
+#include <tasks/task.h>
+#include "freertos/idf_additions.h"
+#include "nvs_flash.h"
+#include <stdio.h>
+#include <string.h>
+
+
 #endif
