0df73cdd60275aaa0cc2b08437c02ee7de1e7ea6
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-10-29 10:15:33 +0100

    Update task.h

--- master_device/main/include/task.h
+++ master_device/main/include/task.h
@@ -1,7 +1,10 @@
-#include "components/components.h"
-#include "components/nvs/nvs.h"
-#include "components/data/data.h" 
-#include "components/wifi/wifi.h"
+#ifndef TASK_HEADER
+#define TASK_HEADER
+
+#include "include/components.h"
+#include "include/data.h"
+#include "include/nvs.h"
+#include "include/wifi.h"
 
 
 #include <freertos/projdefs.h>
@@ -11,18 +14,6 @@
 #include <freertos/idf_additions.h>
 #include <freertos/semphr.h>
 #include <stdint.h>
-
-
-#ifndef TASK_HEADER
-#define TASK_HEADER
-
-#define WIFI_REINIT_INTERVAL (5000 / portTICK_PERIOD_MS)
-
-//
-// typedef struct {
-//     TaskHandle_t wifi_setup; 
-// } my_task_handles_t;
-//
 
 
 #endif
