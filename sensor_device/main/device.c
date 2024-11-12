7275f674938520fd8f30b76e18526986230d9da1
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-11-12 11:13:39 +0100

    Update device.c

--- sensor_device/main/device.c
+++ sensor_device/main/device.c
@@ -1,20 +1,18 @@
 #include "device.h"
 #include "freertos/idf_additions.h"
 #include "nvs/nvs.h"
+#include "tasks/data/data.h"
 
-static char slave_serial_number[SERIAL_NUMBER_SIZE + 1];
 
 void app_main(void)
 {
     ESP_LOGI("TAG", "app_started");
-    init_nvs_partitions();
-    get_slave_serial_number_from_nvs(slave_serial_number);
-
-    ESP_LOGI("TAG", "Slave serial number = %s", slave_serial_number);
-
+    ESP_ERROR_CHECK(memory_setup());
     vTaskDelay(100 / portTICK_PERIOD_MS);
     initialize_sensors();    
     vTaskDelay(100 / portTICK_PERIOD_MS);
+    
     initialize_tasks();
+    
     vTaskSuspend(NULL);
 }
