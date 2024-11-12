7ce7669d4766333a41577d5dfc75b7808e3d481e
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-11-12 11:30:43 +0100

    Update device.c

--- sensor_device/main/device.c
+++ sensor_device/main/device.c
@@ -8,6 +8,9 @@ void app_main(void)
 {
     ESP_LOGI("TAG", "app_started");
     ESP_ERROR_CHECK(memory_setup());
+
+    debug();
+    
     vTaskDelay(100 / portTICK_PERIOD_MS);
     initialize_sensors();    
     vTaskDelay(100 / portTICK_PERIOD_MS);
