f6ff09ac6a7037feaf3219215188bc6086c07594
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-11-12 10:42:41 +0100

    Update data.h

--- sensor_device/main/tasks/data/data.h
+++ sensor_device/main/tasks/data/data.h
@@ -3,12 +3,22 @@
 
 #include <stdint.h>
 
-struct data {
+typedef struct {
+    char serial_number[SERIAL_NUMBER_SIZE];
+    uint8_t mac_address[6];
+} slave_device_t;
+
+typedef struct {
     double lux;
     double humidity;
     double pressure;
     double temperature;
-};
+} sensor_data_t;
+
+typedef struct {
+    bool auto_light;
+    int light_value;
+} light_control_t;
 
 extern double get_lux(void);
 extern double get_temperature(void);
