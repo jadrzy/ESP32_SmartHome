437f8dabb59826750a0c78fc120543eae8df258b
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-11-18 13:59:14 +0100

    Update ntp.c

--- master_device/main/src/ntp.c
+++ master_device/main/src/ntp.c
@@ -41,11 +41,32 @@ void time_sync_cb(struct timeval *tv)
     flags->rtc_synchronized = 1;
 }
 
+#include "esp_http_client.h"
+
+static void http_test_task(void *pvParameters)
+{
+    esp_http_client_config_t config = {
+        .url = "http://example.com",
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+        ESP_LOGI("HTTP", "HTTP GET Status = %d, content_length = %d",
+                 esp_http_client_get_status_code(client),
+                 esp_http_client_get_content_length(client));
+    } else {
+        ESP_LOGE("HTTP", "HTTP GET request failed: %s", esp_err_to_name(err));
+    }
+    esp_http_client_cleanup(client);
+    vTaskDelete(NULL);
+}
+
 
 void synch_time(void)
 {
     ESP_LOGI(TAG_NTP, "Initializing SNTP");
-
+    http_test_task();
     esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
     esp_sntp_setservername(0, "pool.ntp.org");
     sntp_set_time_sync_notification_cb(time_sync_cb);
