c2f9b8ed7fcc619eaee38d3522470d8679e86b36
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-11-18 11:44:57 +0100

    Update ntp.c

--- master_device/main/src/ntp.c
+++ master_device/main/src/ntp.c
@@ -14,35 +14,34 @@
 #include "nvs_flash.h"
 #include "esp_sntp.h"
 
-static const char* TAG_NTP = "NTP";
 
+static const char* TAG_NTP = "NTP";
 
 void time_sync_cb(struct timeval *tv)
 {
-    wifi_flags_t *flags;
-    flags = get_wifi_flags();
+    wifi_flags_t *flags = get_wifi_flags();
     ESP_LOGI(TAG_NTP, "Time synchronized");
     flags->rtc_synchronized = 1;
 }
 
 void synch_time(void)
 {
-    struct timeval tv;
+    ESP_LOGI(TAG_NTP, "Initializing SNTP");
 
     esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
     esp_sntp_setservername(0, "pool.ntp.org");
     sntp_set_time_sync_notification_cb(time_sync_cb);
     sntp_set_sync_mode(SNTP_SYNC_MODE_IMMED);
-    ESP_LOGI(TAG_NTP, "timeval before %d | %d", (int) tv.tv_sec, (int) tv.tv_usec);
-    sntp_sync_time(&tv);
-    ESP_LOGI(TAG_NTP, "timeval after %d | %d", (int) tv.tv_sec, (int) tv.tv_usec);
 
-    settimeofday(&tv, NULL);
+    esp_sntp_init();
 
-    char strftime_buf[64];
-    time_t now = 0;
-    struct tm timeinfo = {0};
-    localtime_r(&now, &timeinfo);
-    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-    ESP_LOGI(TAG_NTP, "The current date/time in Cracow is: %s", strftime_buf);
+    // Opcjonalnie można monitorować czas oczekiwania na synchronizację
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET) {
+        ESP_LOGI(TAG_NTP, "Waiting for time synchronization...");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    ESP_LOGI(TAG_NTP, "Time synchronized: %ld sec | %ld usec", (long)tv.tv_sec, (long)tv.tv_usec);
 }
