2b2a13e2dbb1d3ab8bee38ee3f31cd974dbc9cf5
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-11-18 12:56:01 +0100

    Update ntp.c

--- master_device/main/src/ntp.c
+++ master_device/main/src/ntp.c
@@ -18,6 +18,19 @@
 #include "esp_netif_types.h"
 
 
+#include <string.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "esp_netif_sntp.h"
+#include "sdkconfig.h"
+
 
 static const char* TAG_NTP = "NTP";
 
@@ -29,7 +42,7 @@ void time_sync_cb(struct timeval *tv)
 }
 
 
-
+/*
 void synch_time(void)
 {
     ESP_LOGI(TAG_NTP, "Initializing SNTP");
@@ -51,5 +64,23 @@ void synch_time(void)
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 
+}
+*/
+void synch_time(void)
+{
+    struct timeval tv = {
+        .tv_sec = 1509449941,
+    };
+    struct timezone tz = {
+        0, 0
+    };
+    settimeofday(&tv, &tz);
+
+    /* Start SNTP service */
+    esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG("time.windows.com");
+    esp_netif_sntp_init(&config);
+    if (esp_netif_sntp_sync_wait(pdMS_TO_TICKS(10000)) != ESP_OK) {
+        printf("Failed to update system time, continuing");
+    }
 }
 
