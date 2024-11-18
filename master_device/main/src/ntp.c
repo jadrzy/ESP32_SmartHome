874e7e1311fbc5d92d430b58e22299f511afd4f6
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-11-18 10:08:22 +0100

    Update ntp.c

--- master_device/main/src/ntp.c
+++ master_device/main/src/ntp.c
@@ -1,6 +1,20 @@
 #include "include/ntp.h"
 #include <bits/types/struct_timeval.h>
 #include "include/wifi.h"
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_attr.h"
+#include "esp_sleep.h"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
+#include "esp_sntp.h"
 
 static const char* TAG_NTP = "NTP";
 
@@ -13,7 +27,30 @@ void time_sync_cb(struct timeval *tv)
     flags->rtc_synchronized = 1;
 }
 
-time_t get_timestamp(void)
+void get_timestamp(void)
 {
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, "pool.ntp.org");
+    sntp_set_time_sync_notification_cb(time_sync_cb);
+    sntp_set_sync_mode(SNTP_SYNC_MODE_IMMED);
+    sntp_init();
 
+    time_t now = 0;
+    struct tm timeinfo = { 0 };
+    int retry = 0;
+    const int retry_count = 10;
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
+        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+    time(&now);
+    localtime_r(&now, &timeinfo);
+
+    // Set timezone to Eastern Standard Time and print local time
+    setenv("TZ", "EST5EDT,M3.2.0/2,M11.1.0", 1);
+    tzset();
+    localtime_r(&now, &timeinfo);
+    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+    ESP_LOGI(TAG, "The current date/time in New York is: %s", strftime_buf);
+    
 }
