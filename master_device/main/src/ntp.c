162c149e325848c1b3f2c0ec7ba2a8e7c97c4f1d
jchlopak <jdrzyzga10@mgmail.com>
2024-11-18 13:59:30 +0100

    ntp problem

--- master_device/main/src/ntp.c
+++ master_device/main/src/ntp.c
@@ -42,45 +42,12 @@ void time_sync_cb(struct timeval *tv)
 }
 
 
-/*
 void synch_time(void)
 {
     ESP_LOGI(TAG_NTP, "Initializing SNTP");
 
     esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
-    esp_sntp_setservername(0, "");
-    // sntp_set_time_sync_notification_cb(time_sync_cb);
-    // sntp_set_sync_mode(SNTP_SYNC_MODE_IMMED);
-    // esp_sntp_init();
-    struct timeval tv;
-
-    sntp_sync_time(&tv);
-
-    gettimeofday(&tv, NULL);
-    ESP_LOGI(TAG_NTP, "Time synchronized: %ld sec | %ld usec", (long)tv.tv_sec, (long)tv.tv_usec);
-    // Opcjonalnie można monitorować czas oczekiwania na synchronizację
-    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET) {
-        ESP_LOGI(TAG_NTP, "Waiting for time synchronization...");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-
+    esp_sntp_setservername(0, "pool.ntp.org");
+    sntp_set_time_sync_notification_cb(time_sync_cb);
+    esp_sntp_init();
 }
-*/
-void synch_time(void)
-{
-    struct timeval tv = {
-        .tv_sec = 1509449941,
-    };
-    struct timezone tz = {
-        0, 0
-    };
-    settimeofday(&tv, &tz);
-
-    /* Start SNTP service */
-    esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG("time.windows.com");
-    esp_netif_sntp_init(&config);
-    if (esp_netif_sntp_sync_wait(pdMS_TO_TICKS(10000)) != ESP_OK) {
-        printf("Failed to update system time, continuing");
-    }
-}
-
