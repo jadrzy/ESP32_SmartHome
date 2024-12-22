6124e59149a37334611d34c710d29505b3aa92cf
jchlopak <128635994+jadrzy@users.noreply.github.com>
2024-12-22 20:26:29 +0100

    Update nvs.h

--- master_device/main/include/nvs.h
+++ master_device/main/include/nvs.h
@@ -21,6 +21,7 @@
 // Function declarations
 esp_err_t init_nvs_partitions(void);
 esp_err_t get_master_serial_number_from_nvs(char *number);
+esp_err_t get_http_cred_from_nvs(char *url, char *api_key);
 esp_err_t get_wifi_sm_cred_from_nvs(char * SSID, char * PSSWD);
 esp_err_t write_wifi_sm_cred_to_nvs(char * SSID,char * PSSWD);
 esp_err_t get_paired_devices_from_nvs(uint64_t mac_device_list[NUMBER_OF_DEVICES], char serial_device_list[NUMBER_OF_DEVICES][SERIAL_NUMBER_SIZE]);
